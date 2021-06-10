48c4e022c689b7edfcd0853207c32249bec4f3f0
vikash2806 <vikashrajendran2806@gmail.com>
2021-06-10 08:45:53 +0530

    two players conditional statment added in JS

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ document.addEventListener("DOMContentLoaded", () => {
     const squares = document.querySelectorAll('.grid div');
     const result = document.getElementById('result');
     const displayCurrentPlayer = document.getElementById('current-player');
-    let currentPlayer = "Vikash";
+    let currentPlayer = 1;
 
     for (let i = 0; i < squares.length; i++) {
         squares[i].onclick = () => {
-            alert("hey u got a error!" + i);
+            if (squares[i + 7].classList.contains('taken')) {
+                if (currentPlayer == 1) {
+                    squares[i].classList.add('taken');
+                    squares[i].classList.add('player-one');
+                    currentPlayer = 2;
+                    displayCurrentPlayer.innerHTML = 2;
+                } else if (currentPlayer == 2) {
+                    squares[i].classList.add('taken');
+                    squares[i].classList.add('player-two');
+                    currentPlayer = 1;
+                    displayCurrentPlayer.innerHTML = 1;
+                }
+            } else alert("tf you can't do that");
         }
     }
 })
